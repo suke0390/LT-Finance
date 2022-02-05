a8e8bef2f981da03da668dbd3ef964f97f2d9bae
Suke <archsuke@gmail.com>
2022-02-06 04:24:33 +0800

    Профайл хэсгийг нэмэх

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,12 @@
 const nav__btn = document.getElementById("burger");
 const mobile__menu = document.getElementById("mobile-menu");
+const profile = document.getElementById("profile");
+const info = document.getElementById("info");
 
 nav__btn.addEventListener("click", () => {
   mobile__menu.classList.toggle("hidden");
+});
+
+profile.addEventListener("click", () => {
+  info.classList.toggle("hidden");
 });
